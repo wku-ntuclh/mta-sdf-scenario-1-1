a1358505c6348f4ed0342d35f0b62019761437d0
William Ku <kucheech@gmail.com>
2018-09-08 20:05:55 +0800

    Add project files.

---
+++ Scenario 1-1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scenario_1_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int discount = 0;
+            Console.Write("Enter quantity: ");
+            int qty = int.Parse(Console.ReadLine());
+
+            if(qty < 10)
+            {
+                discount = 5;
+            } else if (qty < 50)
+            {
+                discount = 10;
+            } else
+            {
+                discount = 20;
+            }
+
+            Console.WriteLine("Discount: {0}%", discount);
+        }
+    }
+}
